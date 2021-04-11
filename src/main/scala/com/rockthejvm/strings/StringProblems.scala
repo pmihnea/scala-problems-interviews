f2d89445185f33fbf7e43060d73e62cbb2adee54
Mihnea Pretorian <13440122+pmihnea@users.noreply.github.com>
2021-04-11 18:31:45 +0200

    added strings check anagrams

--- src/main/scala/com/rockthejvm/strings/StringProblems.scala
+++ src/main/scala/com/rockthejvm/strings/StringProblems.scala
@@ -1,8 +1,8 @@
 package com.rockthejvm.strings
 
 object StringProblems extends App {
-  def countCharacters(s: String): Map[Char,Int] = {
-    s.foldLeft(Map[Char,Int]())((m , c) => m.updatedWith(c)(maybeValue => maybeValue.map(_ + 1).orElse(Some(1)))
+  def countCharacters(s: String): Map[Char, Int] = {
+    s.foldLeft(Map[Char, Int]())((m, c) => m.updatedWith(c)(maybeValue => maybeValue.map(_ + 1).orElse(Some(1)))
     )
   }
 
@@ -10,12 +10,34 @@ object StringProblems extends App {
     println(s"countCharacters($s) = " + countCharacters(s))
   }
 
-  def _testSuiteCountCharacters = {
+  def testSuiteCountCharacters = {
     testCountCharacters("")
     testCountCharacters("a")
     testCountCharacters("abc")
     testCountCharacters("abccbaef")
   }
 
+  def checkAnagrams(sa: String, so: String): Boolean = {
+    val saCounters = countCharacters(sa)
+    val finalCounters = so.foldLeft(saCounters)((m, c) => m.updatedWith(c)(maybeCounter => {
+      maybeCounter match {
+        case None => Some(-1)
+        case Some(1) => None
+        case Some(i) => Some(i - 1)
+      }
+    }))
+    finalCounters.isEmpty
+  }
 
+  def testCheckAnagrams(sa: String, so: String) = {
+    println(s"checkAnagrams($sa, $so) = " + checkAnagrams(sa, so))
+  }
+
+  def testSuiteCheckAnagrams = {
+    testCheckAnagrams("abcab", "bacba")
+    testCheckAnagrams("abcab", "bacbac")
+    testCheckAnagrams("Scala", "laSca")
+  }
+
+  testSuiteCheckAnagrams
 }
